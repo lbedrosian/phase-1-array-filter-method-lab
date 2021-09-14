f67ec3cd09f42525e726cd8d2d9f09b359229561
Luke Bedrosian <bedrosian.luke05@gmail.com>
2021-09-14 19:39:02 -0400

    commit tree 92b4fcc835e0fd0f93607035782b36f5662d1933

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solution here
+let friends = ['Teresa','Grecia','Alex','Shannon','Adam',]
+function findMatching(listOfNames,person) {
+    let sameName = listOfNames.filter(function(element) {
+        if (element.toLowerCase() === person.toLowerCase()) {
+            return element;}
+    });
+    return sameName;
+}
+function fuzzyMatch(listOfNames,letters) {
+    let sameLetters = listOfNames.filter(function(element) {
+        return element.startsWith(letters);
+    });
+    return sameLetters;
+};
+function matchName(driverObjects,randomName) {
+    let nameEqual = driverObjects.filter(function(element) {
+        if (element.name === randomName) {
+            return element.name;}
+    });
+    return nameEqual;
+}
